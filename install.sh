79c9997ed91fd2a7a54d888a9c56c0327e2aff65
minecraftwiki55 <minecraftwiki55@gmail.com>
2025-06-06 19:34:13 +0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-9.0
+chmod +x VoiceCraft.Server
+./VoiceCraft.Server
